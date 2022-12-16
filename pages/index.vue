ea799e93219c653ccafef8f624c09b39fb0ab3dc
Kicshikxo <supermegavalera9000@gmail.com>
2022-12-16 12:56:06 +0300

    Increase tabs padding

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="min-h-screen max-h-screen flex flex-column">
+    <div class="flex flex-column min-h-screen surface-ground">
         <div class="h-16rem bg-indigo-500">
             <header
                 class="flex justify-content-between w-full h-4rem py-3 px-5 bg-indigo-500 border-bottom-1 border-indigo-600 shadow-1"
@@ -8,24 +8,24 @@
                     <nuxt-img
                         src="images/logo.webp"
                         alt="logo"
-                        width="40"
-                        height="40"
+                        width="48"
+                        height="48"
                         class="mr-4"
                     />
                     <nuxt-link
-                        class="flex px-3 py-2 text-indigo-100 hover:text-indigo-50 hover:bg-indigo-600 font-medium border-round transition-colors transition-duration-150 cursor-pointer"
+                        class="flex p-3 text-indigo-100 hover:text-indigo-50 hover:bg-indigo-600 font-medium border-round transition-colors transition-duration-150 cursor-pointer"
                     >
                         <i class="pi pi-user w-1rem mr-2"></i>
                         <span>Учащиеся</span>
                     </nuxt-link>
                     <nuxt-link
-                        class="flex align-items-center px-3 py-2 text-indigo-100 hover:text-indigo-50 hover:bg-indigo-600 font-medium border-round transition-colors transition-duration-150 cursor-pointer"
+                        class="flex p-3 text-indigo-100 hover:text-indigo-50 hover:bg-indigo-600 font-medium border-round transition-colors transition-duration-150 cursor-pointer"
                     >
                         <i class="pi pi-users w-1rem mr-2"></i>
                         <span>Классы</span>
                     </nuxt-link>
                     <nuxt-link
-                        class="flex align-items-center px-3 py-2 text-indigo-100 hover:text-indigo-50 hover:bg-indigo-600 font-medium border-round transition-colors transition-duration-150 cursor-pointer"
+                        class="flex p-3 text-indigo-100 hover:text-indigo-50 hover:bg-indigo-600 font-medium border-round transition-colors transition-duration-150 cursor-pointer"
                     >
                         <i class="pi pi-chart-line w-1rem mr-2"></i>
                         <span>Статистика</span>
@@ -33,7 +33,7 @@
                 </div>
                 <div class="flex align-items-center">
                     <nuxt-link
-                        class="flex p-2 mr-2 align-items-center text-indigo-100 hover:text-indigo-50 hover:bg-indigo-600 font-medium border-round transition-colors transition-duration-150 cursor-pointer"
+                        class="flex p-3 mr-2 align-items-center text-indigo-100 hover:text-indigo-50 hover:bg-indigo-600 font-medium border-round transition-colors transition-duration-150 cursor-pointer"
                     >
                         <i class="pi pi-bell text-xl" />
                     </nuxt-link>
