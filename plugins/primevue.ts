2a13b5488fb28fffa5a675d691205d32c70ca76f
Kicshikxo <supermegavalera9000@gmail.com>
2022-12-17 22:58:57 +0300

    Add DataTable and Dialog components

--- plugins/primevue.ts
+++ plugins/primevue.ts
@@ -2,9 +2,15 @@ import { defineNuxtPlugin } from '#app'
 import PrimeVue from 'primevue/config'
 import Button from 'primevue/button'
 import InputText from 'primevue/inputtext'
+import DataTable from 'primevue/datatable'
+import Column from 'primevue/column'
+import Dialog from 'primevue/dialog'
 
 export default defineNuxtPlugin((nuxtApp) => {
     nuxtApp.vueApp.use(PrimeVue, { ripple: false })
     nuxtApp.vueApp.component('PButton', Button)
     nuxtApp.vueApp.component('PInputText', InputText)
+    nuxtApp.vueApp.component('PDataTable', DataTable)
+    nuxtApp.vueApp.component('PColumn', Column)
+    nuxtApp.vueApp.component('PDialog', Dialog)
 })
