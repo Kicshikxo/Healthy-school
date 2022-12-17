4228f7fe6bf574c254a675e8d7fdf08980c20fca
Kicshikxo <supermegavalera9000@gmail.com>
2022-12-17 22:57:36 +0300

    Add breadcrumbs in app nav

--- components/AppNav.vue
+++ components/AppNav.vue
@@ -1,28 +1,55 @@
 <template>
     <nav
-        class="flex align-items-center gap-3 h-4rem py-3 px-5 font-medium overflow-x-auto overflow-y-hidden"
+        class="flex align-items-center gap-3 h-4rem py-3 mx-5 font-medium overflow-x-auto overflow-y-hidden"
     >
-        <nuxt-link class="text-indigo-200 hover:text-indigo-100 cursor-pointer">
+        <nuxt-link
+            to="/"
+            :class="
+                $route.path == '/'
+                    ? 'text-indigo-50'
+                    : 'text-indigo-200 hover:text-indigo-100'
+            "
+            class="no-underline"
+        >
             <i class="pi pi-home w-1rem text-xl"></i>
         </nuxt-link>
-        <i class="pi pi-angle-right w-1rem text-indigo-200"></i>
 
-        <nuxt-link
-            class="text-indigo-200 hover:text-indigo-100 white-space-nowrap cursor-pointer"
-        >
-            Учащиеся
-        </nuxt-link>
-        <i class="pi pi-angle-right w-1rem text-indigo-200"></i>
-
-        <nuxt-link
-            class="text-indigo-200 hover:text-indigo-100 white-space-nowrap cursor-pointer"
-        >
-            Иванов Иван Иванович
-        </nuxt-link>
-        <i class="pi pi-angle-right w-1rem text-indigo-200"></i>
-
-        <nuxt-link class="text-indigo-50 white-space-nowrap cursor-pointer">
-            Сведения
-        </nuxt-link>
+        <div v-for="route in breadcrumbs" class="flex align-items-center gap-3">
+            <i class="pi pi-angle-right w-1rem text-indigo-200"></i>
+            <nuxt-link
+                :to="route.path"
+                :class="
+                    $route.path == route.path
+                        ? 'text-indigo-50'
+                        : 'text-indigo-200 hover:text-indigo-100'
+                "
+                class="no-underline"
+            >
+                {{ route.name }}
+            </nuxt-link>
+        </div>
     </nav>
 </template>
+
+<script setup lang="ts">
+const shortcuts = new Map<string, string>()
+shortcuts.set('login', 'Логин')
+shortcuts.set('profile', 'Профиль')
+shortcuts.set('children', 'Учащиеся')
+
+const route = useRoute()
+
+const breadcrumbs = computed(() =>
+    route.path
+        .split('/')
+        .slice(1)
+        .reduce(
+            (acc, path, index, paths) =>
+                acc.concat({
+                    name: shortcuts.get(path) ?? path,
+                    path: `/${paths.slice(0, index + 1).join('/')}`
+                }),
+            [] as { name: string; path: string }[]
+        )
+)
+</script>
