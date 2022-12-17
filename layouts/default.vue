6ff1f750a74e790fa900835c8ded472ca58f0994
Kicshikxo <supermegavalera9000@gmail.com>
2022-12-17 22:58:11 +0300

    Optimize default layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -5,11 +5,11 @@
             <app-nav />
         </div>
 
-        <main class="flex flex-auto px-5 mb-5" style="margin-top: -8rem">
+        <main class="flex flex-auto mx-5 mb-5" style="margin-top: -8rem">
             <div
-                class="flex-auto relative p-2 border-2 surface-border border-round surface-section shadow-2 overflow-x-hidden overflow-y-auto"
+                class="flex-auto relative border-2 surface-border border-round surface-section shadow-2 overflow-x-hidden overflow-y-auto"
             >
-                <div class="absolute">
+                <div class="absolute w-full min-h-full">
                     <slot />
                 </div>
             </div>
