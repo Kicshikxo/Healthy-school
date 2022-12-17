e78a5d34d26f2b3a18b91cdb0ce9b11a56604b5c
Kicshikxo <supermegavalera9000@gmail.com>
2022-12-17 23:01:33 +0300

    Change api endpoints

--- server/api/children/index.ts
+++ server/api/children/index.ts
@@ -2,5 +2,5 @@ import { PrismaClient } from '@prisma/client'
 const prisma = new PrismaClient()
 
 export default defineEventHandler(async (event) => {
-    return await prisma.user.findMany()
+    return await prisma.child.findMany({ orderBy: { secondName: 'asc' } })
 })
