d29815a3d8a49c9672911543cfbe799ead22cde9
Kicshikxo <supermegavalera9000@gmail.com>
2022-12-18 21:20:31 +0300

    Add horizontal scrolling for layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -12,7 +12,7 @@
 
         <main class="flex flex-auto mx-5 mb-5" style="margin-top: -8rem">
             <div
-                class="flex-auto relative border-2 surface-border border-round surface-section shadow-2 overflow-x-hidden overflow-y-auto"
+                class="flex-auto relative border-2 surface-border border-round surface-section shadow-2 overflow-x-auto overflow-y-auto"
             >
                 <div class="absolute w-full min-h-full">
                     <slot />
