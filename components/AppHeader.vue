5a46eee13e61e8f8136de2070c7149a2805c052f
Kicshikxo <supermegavalera9000@gmail.com>
2022-12-18 21:19:48 +0300

    Remove classes link from header

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -20,12 +20,6 @@
             <nuxt-link
                 class="flex p-3 text-indigo-100 hover:text-indigo-50 hover:bg-indigo-600 font-medium border-round transition-colors transition-duration-150 cursor-pointer no-underline"
             >
-                <i class="pi pi-users w-1rem mr-2"></i>
-                <span>Классы</span>
-            </nuxt-link>
-            <nuxt-link
-                class="flex p-3 text-indigo-100 hover:text-indigo-50 hover:bg-indigo-600 font-medium border-round transition-colors transition-duration-150 cursor-pointer no-underline"
-            >
                 <i class="pi pi-chart-line w-1rem mr-2"></i>
                 <span>Статистика</span>
             </nuxt-link>
