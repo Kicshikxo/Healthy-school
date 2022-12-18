d99357e2956541e15f3c5196e103e135273aa62c
Kicshikxo <supermegavalera9000@gmail.com>
2022-12-19 00:00:35 +0300

    Add importing primevue components as sfc

--- plugins/primevue.ts
+++ plugins/primevue.ts
@@ -1,11 +1,12 @@
 import { defineNuxtPlugin } from '#app'
 import PrimeVue from 'primevue/config'
-import Button from 'primevue/button'
-import InputText from 'primevue/inputtext'
-import DataTable from 'primevue/datatable'
-import Column from 'primevue/column'
-import Dialog from 'primevue/dialog'
-import Skeleton from 'primevue/skeleton'
+import Button from 'primevue/button/Button.vue'
+import InputText from 'primevue/inputtext/InputText.vue'
+import DataTable from 'primevue/datatable/DataTable.vue'
+import Column from 'primevue/column/Column.vue'
+import Dialog from 'primevue/dialog/Dialog.vue'
+import Skeleton from 'primevue/skeleton/Skeleton.vue'
+import Dropdown from 'primevue/dropdown/Dropdown.vue'
 
 export default defineNuxtPlugin((nuxtApp) => {
     nuxtApp.vueApp.use(PrimeVue, { ripple: false })
@@ -15,4 +16,5 @@ export default defineNuxtPlugin((nuxtApp) => {
     nuxtApp.vueApp.component('PColumn', Column)
     nuxtApp.vueApp.component('PDialog', Dialog)
     nuxtApp.vueApp.component('PSkeleton', Skeleton)
+    nuxtApp.vueApp.component('PDropdown', Dropdown)
 })
