600e78f9c3990e1a45aa06d048c16dba0f654639
Kicshikxo <supermegavalera9000@gmail.com>
2022-12-18 16:54:36 +0300

    Add login page route name

--- pages/login.vue
+++ pages/login.vue
@@ -1,5 +1,14 @@
 <template>
-    <div class="flex justify-content-center align-items-center h-screen w-screen">
+    <div
+        class="flex justify-content-center align-items-center h-screen w-screen"
+    >
         <login-form />
     </div>
 </template>
+
+<script setup lang="ts">
+definePageMeta({
+    name: 'Логин',
+    layout: false
+})
+</script>
