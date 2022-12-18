54b95b6e063726488fb9561279d6f936b30920e3
Kicshikxo <supermegavalera9000@gmail.com>
2022-12-18 21:20:08 +0300

    Change required meta field from name to title

--- components/AppNav.vue
+++ components/AppNav.vue
@@ -29,7 +29,7 @@
                 "
                 class="no-underline"
             >
-                {{ $router.resolve({ path }).meta.name ?? $route.path }}
+                {{ $router.resolve({ path }).meta.title ?? $route.path }}
             </nuxt-link>
         </div>
     </nav>
