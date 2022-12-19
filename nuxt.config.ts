746280db2573d9455c3b2c485eddd01bcbb7aca3
Kicshikxo <supermegavalera9000@gmail.com>
2022-12-19 21:45:09 +0300

    Change page title

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,7 +4,7 @@ export default defineNuxtConfig({
             htmlAttrs: {
                 lang: 'ru'
             },
-            title: 'Школьное здоровье',
+            title: 'Здоровье в школе',
             meta: [
                 {
                     hid: 'description',
