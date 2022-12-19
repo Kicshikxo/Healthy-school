659141b8966b3d2b7a80180200f8d2cdd697033b
Kicshikxo <supermegavalera9000@gmail.com>
2022-12-19 11:44:48 +0300

    Add file extensions for sfc import

--- plugins/primevue.ts
+++ plugins/primevue.ts
@@ -1,15 +1,15 @@
 import { defineNuxtPlugin } from '#app'
 import PrimeVue from 'primevue/config'
-import Button from 'primevue/button/Button'
-import InputText from 'primevue/inputtext/InputText'
-import DataTable from 'primevue/datatable/DataTable'
-import Column from 'primevue/column/Column'
-import Dialog from 'primevue/dialog/Dialog'
-import Skeleton from 'primevue/skeleton/Skeleton'
-import Dropdown from 'primevue/dropdown/Dropdown'
+import Button from 'primevue/button/Button.vue'
+import InputText from 'primevue/inputtext/InputText.vue'
+import DataTable from 'primevue/datatable/DataTable.vue'
+import Column from 'primevue/column/Column.vue'
+import Dialog from 'primevue/dialog/Dialog.vue'
+import Skeleton from 'primevue/skeleton/Skeleton.vue'
+import Dropdown from 'primevue/dropdown/Dropdown.vue'
 
 export default defineNuxtPlugin((nuxtApp) => {
-    nuxtApp.vueApp.use(PrimeVue, { ripple: false })
+    nuxtApp.vueApp.use(PrimeVue, { ripple: true })
     nuxtApp.vueApp.component('PButton', Button)
     nuxtApp.vueApp.component('PInputText', InputText)
     nuxtApp.vueApp.component('PDataTable', DataTable)
