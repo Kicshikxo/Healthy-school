85f5ea148b00cc51420e67ca3ecc8174d17db58d
Kicshikxo <supermegavalera9000@gmail.com>
2022-12-27 01:56:49 +0300

    Fix auth middleware for ssr

--- middleware/auth.global.ts
+++ middleware/auth.global.ts
@@ -1,8 +1,9 @@
 export default defineNuxtRouteMiddleware(async (to) => {
     if (to.meta.auth === false) return
 
-    const { state } = useSession()
+    const { state, getSession } = useSession()
 
+    if (state.status.value === 'unauthenticated') await getSession()
     if (state.status.value === 'authenticated') return
 
     return navigateTo('/login')
