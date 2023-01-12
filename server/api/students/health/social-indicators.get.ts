9e15079098c6ebff883f67dfc71bc1b1718dcb2d
Kicshikxo <supermegavalera9000@gmail.com>
2023-01-12 12:46:16 +0300

    Rename routes

--- server/api/students/health/social-indicators.get.ts
+++ server/api/students/health/social-indicators.get.ts
@@ -2,5 +2,7 @@ import { PrismaClient } from '@prisma/client'
 const prisma = new PrismaClient()
 
 export default defineEventHandler(async (event) => {
-    return await prisma.socialHealthIndicator.findMany()
+    return await prisma.socialHealthIndicator.findMany({
+        orderBy: [{ healthZone: 'desc' }, { title: 'asc' }]
+    })
 })
