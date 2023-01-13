46e0f7a49e43961b7144c5c5f82e10c52a7441af
Kicshikxo <supermegavalera9000@gmail.com>
2023-01-13 19:57:11 +0300

    Change prisma schema and add seeding

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,43 +1,108 @@
-import crc32 from 'crc/crc32'
 import { hashSync } from 'bcrypt'
 import { PrismaClient } from '@prisma/client'
-import { socialHealthIndicators, userData } from './seedData'
+import {
+    municipality,
+    educationalOrganization,
+    usersData,
+    socialHealthIndicators,
+    physicalHealthRecommendations,
+    socialHealthRecommendations
+} from './seedData'
 const prisma = new PrismaClient()
 
 async function main() {
+    let municipalityId: string
     try {
-        await prisma.user.create({
-            data: {
-                id: crc32(userData.username).toString(16),
-                username: userData.username,
-                password: hashSync(userData.password, 8),
-                role: userData.role,
+        municipalityId = (
+            await prisma.municipality.create({
+                data: {
+                    name: municipality.name
+                }
+            })
+        ).id
+        console.log('Создано базовое муниципальное образование')
+    } catch (e) {
+        municipalityId = (await prisma.municipality.findUnique({
+            where: {
+                name: municipality.name
+            }
+        }))!.id
+        console.log('Базовое муниципальное образование уже добавлено')
+    }
+
+    let organizationId: string
+    try {
+        organizationId = (
+            await prisma.educationalOrganization.create({
+                data: {
+                    name: educationalOrganization.name,
+                    municipality: {
+                        connect: {
+                            id: municipalityId
+                        }
+                    }
+                }
+            })
+        ).id
+        console.log('Создана базовая образовательная организация')
+    } catch (e) {
+        organizationId = (await prisma.educationalOrganization.findUnique({
+            where: {
+                name: educationalOrganization.name
+            }
+        }))!.id
+        console.log('Базовая образовательая организаця уже создана')
+    }
+
+    try {
+        await prisma.user.createMany({
+            data: usersData.map((user) => ({
+                username: user.username,
+                password: hashSync(user.password, 8),
+                role: user.role,
+
+                organizationId: organizationId,
 
                 secondName: 'Иванов',
                 firstName: 'Иван',
                 middleName: 'Иванович'
-            }
+            }))
         })
 
-        console.log(
-            `Создан базовый пользователь для работы с системой:\n\tЛогин: ${userData.username}\n\tПароль: ${userData.password}\n\tРоль: ${userData.role}`
-        )
-    } catch (e) {
-        console.log(
-            `Базовый пользователь для работы с системой уже создан (${userData.username}:${userData.password}) или произошла ошибка добавления`
-        )
-    }
+        console.log(`Созданы базовые пользователи для работы с системой`)
+    } catch (e) {}
 
     const socialHealthIndicatorsCount = await prisma.socialHealthIndicator.count()
-
     if (socialHealthIndicatorsCount === 0) {
         await prisma.socialHealthIndicator.createMany({
             data: socialHealthIndicators as any
         })
 
-        console.log(`Создано ${socialHealthIndicators.length} социальных индикаторов здоровья`)
+        console.log(`Создано ${socialHealthIndicators.length} индикаторов социального здоровья`)
     } else {
         console.log(`В БД уже хранится ${socialHealthIndicatorsCount} социальных индикаторов здоровья`)
+    }
+
+    const physicalHealthRecommendationsCount = await prisma.physicalHealthRecommendation.count()
+    if (physicalHealthRecommendationsCount === 0) {
+        await prisma.physicalHealthRecommendation.createMany({
+            data: physicalHealthRecommendations as any
+        })
+
+        console.log(`Создано ${physicalHealthRecommendations.length} рекомендаций по физическому здоровью`)
+    } else {
+        console.log(`В БД уже хранится ${physicalHealthRecommendationsCount} рекомендаций по физическому здоровью`)
+    }
+
+    const socialHealthRecommendationsCount = await prisma.socialHealthRecommendation.count()
+    if (socialHealthRecommendationsCount === 0) {
+        await prisma.socialHealthRecommendation.createMany({
+            data: socialHealthRecommendations as any
+        })
+
+        console.log(`Создано ${socialHealthRecommendations.length} рекомендаций по социальному здоровью`)
+    } else {
+        console.log(`В БД уже хранится ${socialHealthRecommendationsCount} рекомендаций по социальному здоровью`)
     }
 }
 
--- prisma/seedData.ts
+++ prisma/seedData.ts
@@ -1,12 +1,116 @@
-import { HealthZone, Role } from '@prisma/client'
+import { Role, HealthZone, HealthGroup } from '@prisma/client'
 
-export const userData = {
-    username: 'admin',
-    password: 'admin',
-    role: Role.OPERATOR
+export const municipality: { name: string } = {
+    name: 'Курский район'
 }
 
-export const socialHealthIndicators = [
+export const educationalOrganization: { name: string } = {
+    name: 'СОШ с углубленным изучением предметов художественно-эстетического цикла № 27 имени А. А. Дейнеки'
+}
+
+export const usersData: { username: string; password: string; role: Role }[] = [
+    {
+        username: 'admin',
+        password: 'test',
+        role: Role.OPERATOR
+    },
+    {
+        username: 'class',
+        password: 'test',
+        role: Role.CLASS_TEACHER
+    },
+    {
+        username: 'health',
+        password: 'test',
+        role: Role.HEALTH_WORKER
+    },
+    {
+        username: 'social',
+        password: 'test',
+        role: Role.SOCIAL_PEDAGOGUE
+    },
+    {
+        username: 'phys',
+        password: 'test',
+        role: Role.PHYSICAL_EDUCATION_TEACHER
+    },
+    {
+        username: 'ped',
+        password: 'test',
+        role: Role.PEDAGOGUE
+    }
+]
+
+export const physicalHealthRecommendations: { title: string; healthGroup: HealthGroup }[] = [
+    {
+        title: 'выполнение программы физического воспитания с ограничениями по рекомендации врача',
+        healthGroup: HealthGroup.PREPARATORY
+    },
+    {
+        title: 'сдача индивидуальных нормативов и участие в массовых физкультурных мероприятиях при наличии медицинского заключения по результатам дополнительного осмотра',
+        healthGroup: HealthGroup.PREPARATORY
+    },
+    { title: 'не допущение к занятиям спортом и участию в соревнованиях', healthGroup: HealthGroup.PREPARATORY },
+    {
+        title: 'проведение дополнительных занятий для повышения общей физической подготовки',
+        healthGroup: HealthGroup.PREPARATORY
+    },
+
+    {
+        title: 'обучение умению и навыкам элементов ЗОЖ',
+        healthGroup: HealthGroup.SPECIAL
+    },
+    {
+        title: 'осуществление самоконтроля здоровья и функциональных возможностей',
+        healthGroup: HealthGroup.SPECIAL
+    },
+    {
+        title: 'организация физкультурных занятий по особой программе, снижение нормативов, строгое дозирование физической нагрузки',
+        healthGroup: HealthGroup.SPECIAL
+    },
+    {
+        title: 'включение лечебной физкультуры в программу занятий',
+        healthGroup: HealthGroup.SPECIAL
+    },
+    {
+        title: 'широкое использование дыхательных, корригирующих и общеразвивающих упражнений',
+        healthGroup: HealthGroup.SPECIAL
+    },
+    {
+        title: 'проявление повышенной осторожности при использовании физических упражнений',
+        healthGroup: HealthGroup.SPECIAL
+    },
+    {
+        title: 'исключение физических упражнений, противопоказанных по состоянию здоровья',
+        healthGroup: HealthGroup.SPECIAL
+    },
+    {
+        title: 'включение в занятия подвижных игр умеренной интенсивности',
+        healthGroup: HealthGroup.SPECIAL
+    }
+]
+
+export const socialHealthIndicators: { title: string; healthZone: HealthZone }[] = [
+    {
+        title: 'Ребенок из семьи с низким уровнем дохода',
+        healthZone: HealthZone.YELLOW
+    },
+    {
+        title: 'Ребенок из семьи переселенцев, беженцев',
+        healthZone: HealthZone.YELLOW
+    },
+    {
+        title: 'Ребенок из семьи участника СВО',
+        healthZone: HealthZone.YELLOW
+    },
+    {
+        title: 'Ребенок из семьи родителей-инвалидов',
+        healthZone: HealthZone.YELLOW
+    },
+    {
+        title: 'Ребенок из семьи с одним родителем',
+        healthZone: HealthZone.YELLOW
+    },
     {
         title: 'Обучающийся является ребенком-инвалидом',
         healthZone: HealthZone.RED
@@ -62,25 +166,89 @@ export const socialHealthIndicators = [
     {
         title: 'Физические наказания, факты насилия, жестокого обращения в семье',
         healthZone: HealthZone.RED
-    },
+    }
+]
+
+export const socialHealthRecommendations: { title: string; healthZone: HealthZone }[] = [
     {
-        title: 'Ребенок из семьи с низким уровнем дохода',
+        title: 'консультирование у специалистов по запросу',
         healthZone: HealthZone.YELLOW
     },
     {
-        title: 'Ребенок из семьи переселенцев, беженцев',
+        title: 'формирование здорового образа жизни через проведение профилактических и просветительских мероприятий',
         healthZone: HealthZone.YELLOW
     },
     {
-        title: 'Ребенок из семьи участника СВО',
+        title: 'занятия с социальным педагогом',
         healthZone: HealthZone.YELLOW
     },
     {
-        title: 'Ребенок из семьи родителей-инвалидов',
+        title: 'социальная помощь',
         healthZone: HealthZone.YELLOW
     },
     {
-        title: 'Ребенок из семьи с одним родителем',
+        title: 'юридическая консультация',
         healthZone: HealthZone.YELLOW
+    },
+    {
+        title: 'просветительские и профилактические занятия с педагогом-психологом',
+        healthZone: HealthZone.YELLOW
+    },
+
+    {
+        title: 'консультирование у специалистов по запросу',
+        healthZone: HealthZone.RED
+    },
+    {
+        title: 'формирование здорового образа жизни через проведение профилактических и просветительских мероприятий',
+        healthZone: HealthZone.RED
+    },
+    {
+        title: 'социальная помощь',
+        healthZone: HealthZone.RED
+    },
+    {
+        title: 'юридическая консультация',
+        healthZone: HealthZone.RED
+    },
+    {
+        title: 'просветительские и профилактические занятия с педагогом-психологом',
+        healthZone: HealthZone.RED
+    },
+    {
+        title: 'контроль и консультирование семьи несовершеннолетнего и обучающегося',
+        healthZone: HealthZone.RED
+    },
+    {
+        title: 'организация межведомственного взаимодействия по сопровождению обучающегося со всеми заинтересованными структурами',
+        healthZone: HealthZone.RED
+    },
+    {
+        title: 'проведение индивидуальных занятий с обучающимся с социальным педагогом и педагогом-психологом',
+        healthZone: HealthZone.RED
+    },
+    {
+        title: 'проведение мониторингов социальной работы в образовательной организации',
+        healthZone: HealthZone.RED
+    },
+    {
+        title: 'организация рейдов в семью совместно с органами опеки, КДН, ПДН',
+        healthZone: HealthZone.RED
+    },
+    {
+        title: 'междисциплинарное взаимодействие с педагогами образовательной организации',
+        healthZone: HealthZone.RED
+    },
+    {
+        title: 'совестная профилактическая и просветительская деятельность с центром сопровождения несовершеннолетних (для семьи – бывших воспитанников школы-интерната)',
+        healthZone: HealthZone.RED
+    },
+    {
+        title: 'проведение мероприятий совместно с медицинскими организациями по профилактике нарко- и алкозависимости',
+        healthZone: HealthZone.RED
+    },
+    {
+        title: 'проведение мероприятий совместно с УМВД по профилактике насилия и жестокости',
+        healthZone: HealthZone.RED
     }
 ]
