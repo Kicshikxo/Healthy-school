f2bf9aace417a119e889c509ce155d9011b934dc
Kicshikxo <supermegavalera9000@gmail.com>
2023-01-17 15:01:59 +0300

    Add health zone indicator component

---
+++ components/health/zone-indicator.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="flex align-items-center" :class="`gap-${gap}`">
+        <div
+            class="border-round"
+            :class="{
+                'health-zone-indicator': healthZone,
+                'bg-green-500': healthZone === 'GREEN',
+                'bg-yellow-500': healthZone === 'YELLOW',
+                'bg-red-500': healthZone === 'RED'
+            }"
+        />
+        <div>
+            {{ label }}
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+import { HealthZone } from '@prisma/client'
+
+withDefaults(
+    defineProps<{
+        healthZone?: HealthZone
+        label?: string
+        gap?: number
+    }>(),
+    {
+        gap: 2
+    }
+)
+</script>
+
+<style>
+.health-zone-indicator {
+    min-width: 6px;
+    height: 20px;
+}
+</style>
