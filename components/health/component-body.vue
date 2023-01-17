7f5280ce7514c19883e73cd7de9e18188a7c931e
Kicshikxo <supermegavalera9000@gmail.com>
2023-01-17 15:00:50 +0300

    Add padding props

--- components/health/component-body.vue
+++ components/health/component-body.vue
@@ -1,7 +1,26 @@
 <template>
-    <p-card class="health-component-body p-card-content-pb-0 border-1 border-300 shadow-none">
+    <p-card
+        class="health-component-body border-1 border-300 shadow-none"
+        :class="{
+            'p-card-content-pb-0': !paddingBottom,
+            'p-card-content-pt-0': !paddingTop
+        }"
+    >
         <template v-if="$slots.title" #title> <slot name="title"></slot> </template>
         <template v-if="$slots.content" #content> <slot name="content"></slot> </template>
         <template v-if="$slots.footer" #footer> <slot name="footer"></slot> </template>
     </p-card>
 </template>
+
+<script setup lang="ts">
+withDefaults(
+    defineProps<{
+        paddingBottom?: boolean
+        paddingTop?: boolean
+    }>(),
+    {
+        paddingBottom: false,
+        paddingTop: true
+    }
+)
+</script>
