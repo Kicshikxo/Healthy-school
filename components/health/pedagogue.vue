0a974d93de5741a5e422072770c48eca84e7cce4
Kicshikxo <supermegavalera9000@gmail.com>
2023-01-18 10:09:47 +0300

    Add pedagogue component base template

---
+++ components/health/pedagogue.vue
@@ -0,0 +1,30 @@
+<template>
+    <p-tab-view>
+        <p-tab-panel header="Педагогические показатели" :disabled="loadingData">
+            <health-component :loading="loadingData" :allow-save="true" :on-cancel="cancelChanges" :on-save="saveChanges">
+                <template #title> Педагогические показатели </template>
+            </health-component>
+        </p-tab-panel>
+        <p-tab-panel header="Логопедические показатели" :disabled="loadingData">
+            <health-component :loading="loadingData" :allow-save="true" :on-cancel="cancelChanges" :on-save="saveChanges">
+                <template #title> Логопедические показатели </template>
+            </health-component>
+        </p-tab-panel>
+    </p-tab-view>
+</template>
+
+<script setup lang="ts">
+const props = defineProps<{
+    studentData: HealthComponentData
+    loadingData: boolean
+    refreshData: () => Promise<void>
+}>()
+
+async function cancelChanges() {
+    await props.refreshData()
+}
+
+async function saveChanges() {
+    await props.refreshData()
+}
+</script>
