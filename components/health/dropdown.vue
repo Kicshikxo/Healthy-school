31bce17fffc3e0f679a70b499934f9ec0b27b5ac
Kicshikxo <supermegavalera9000@gmail.com>
2023-01-23 00:53:19 +0300

    Add health-dropdown component

---
+++ components/health/dropdown.vue
@@ -0,0 +1,43 @@
+<template>
+    <p-dropdown
+        :disabled="disabled"
+        :loading="loading"
+        :options="options"
+        :placeholder="title"
+        :modelValue="modelValue"
+        @update:modelValue="$emit('update:modelValue', $event)"
+        class="w-full"
+        panelClass="border-1 border-300"
+    >
+        <template #value="{ value }">
+            <health-zone-indicator
+                :health-zone="indicator ? value.healthZone : undefined"
+                :label="optionLabel ? value[optionLabel] : value"
+            />
+        </template>
+        <template #option="{ option }">
+            <health-zone-indicator
+                :health-zone="indicator ? option.healthZone : undefined"
+                :label="optionLabel ? option[optionLabel] : option"
+                :gap="3"
+            />
+        </template>
+    </p-dropdown>
+</template>
+
+<script setup lang="ts">
+withDefaults(
+    defineProps<{
+        disabled?: boolean
+        loading?: boolean
+        indicator?: boolean
+        options?: any[]
+        optionLabel?: string
+        modelValue?: any
+        title?: string
+    }>(),
+    {
+        indicator: true
+    }
+)
+</script>
