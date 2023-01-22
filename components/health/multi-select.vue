13916b927fb274116c97a1e696ae0f07af03f3ba
Kicshikxo <supermegavalera9000@gmail.com>
2023-01-23 00:53:28 +0300

    Add health-multi-select component

---
+++ components/health/multi-select.vue
@@ -0,0 +1,44 @@
+<template>
+    <p-multi-select
+        :disabled="disabled"
+        :loading="loading"
+        :options="options"
+        :placeholder="title"
+        :modelValue="modelValue"
+        @update:modelValue="$emit('update:modelValue', $event)"
+        display="chip"
+        class="w-full"
+        appendTo="self"
+        panelClass="border-1 border-300"
+    >
+        <template #chip="{ value }">
+            <health-zone-indicator
+                :health-zone="indicator ? value.healthZone : undefined"
+                :label="optionLabel ? value[optionLabel] : value"
+            />
+        </template>
+        <template #option="{ option }">
+            <health-zone-indicator
+                :health-zone="indicator ? option.healthZone : undefined"
+                :label="optionLabel ? option[optionLabel] : option"
+            />
+        </template>
+    </p-multi-select>
+</template>
+
+<script setup lang="ts">
+withDefaults(
+    defineProps<{
+        disabled?: boolean
+        loading?: boolean
+        indicator?: boolean
+        options?: any[]
+        optionLabel?: string
+        modelValue?: any
+        title?: string
+    }>(),
+    {
+        indicator: true
+    }
+)
+</script>
