349670891426fe35cf898e9e624eeee5196ba8ed
Kicshikxo <supermegavalera9000@gmail.com>
2023-01-24 22:41:22 +0300

    Add conclusions endpoint

---
+++ server/api/conclusions/index.get.ts
@@ -0,0 +1,13 @@
+import { ConclusionType, PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
+
+export default defineEventHandler(async (event) => {
+    const query = getQuery(event) as { type?: ConclusionType }
+
+    return await prisma.conclusion.findMany({
+        where: {
+            conclusionType: query.type
+        }
+    })
+})
