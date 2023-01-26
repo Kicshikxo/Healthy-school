9445f03db9e6c57192fc9d9a174c6b307fb99e36
Kicshikxo <supermegavalera9000@gmail.com>
2023-01-26 20:03:58 +0300

    Add psychological options with recommendations

---
+++ server/api/students/health/psychological/options/with-recommendations.get.ts
@@ -0,0 +1,11 @@
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
+
+export default defineEventHandler(async (event) => {
+    return await prisma.psychologicalHealthOption.findMany({
+        include: {
+            recommendations: true
+        }
+    })
+})
