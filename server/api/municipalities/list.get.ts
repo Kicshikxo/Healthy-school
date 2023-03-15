d5fa9a580315e2ccce2af7c89277f81320e3ceb1
Kicshikxo <supermegavalera9000@gmail.com>
2023-03-15 07:20:34 +0300

    Extend municipality query

--- server/api/municipalities/list.get.ts
+++ server/api/municipalities/list.get.ts
@@ -3,5 +3,31 @@ import { PrismaClient } from '@prisma/client'
 const prisma = new PrismaClient()
 
 export default defineEventHandler(async (event) => {
-    return prisma.municipality.findMany()
+    return prisma.municipality.findMany({
+        include: {
+            _count: {
+                select: {
+                    organizations: true
+                }
+            },
+            organizations: {
+                select: {
+                    _count: {
+                        select: {
+                            classes: true
+                        }
+                    },
+                    classes: {
+                        select: {
+                            _count: {
+                                select: {
+                                    students: true
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    })
 })
