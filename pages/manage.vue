420f070ed6740bb034c4cffcea6fc20d3208923d
Kicshikxo <supermegavalera9000@gmail.com>
2023-03-24 08:13:34 +0300

    Add manage page

---
+++ pages/manage.vue
@@ -0,0 +1,16 @@
+<template>
+    <p-tab-view v-model="activeTabIndex">
+        <p-tab-panel header="Обзор"> </p-tab-panel>
+        <p-tab-panel header="Добавление данных"> </p-tab-panel>
+        <p-tab-panel header="Редактирование данных"> </p-tab-panel>
+        <p-tab-panel header="Удаление данных"> </p-tab-panel>
+    </p-tab-view>
+</template>
+
+<script setup lang="ts">
+definePageMeta({
+    title: 'Управление школой'
+})
+
+const activeTabIndex = ref<number>()
+</script>
