c4294dc692eeac5ed48acb53ba24b6a930e98c1e
Kicshikxo <supermegavalera9000@gmail.com>
2023-03-26 21:08:38 +0300

    Add check available student endpoint

---
+++ server/api/students/check-available.get.ts
@@ -0,0 +1,26 @@
+import { PrismaClient, Role } from '@prisma/client'
+
+const prisma = new PrismaClient()
+
+export default defineEventHandler(async (event) => {
+    if (!checkRole(event, { roles: [Role.OPERATOR, Role.SCHOOL_OPERATOR] })) return
+
+    const query = getQuery(event) as { snils: string }
+
+    if (!query.snils)
+        return sendError(
+            event,
+            createError({
+                statusCode: 400,
+                statusMessage: 'snils is not provided'
+            })
+        )
+
+    return {
+        available: !(await prisma.student.findUnique({
+            where: {
+                snils: query.snils
+            }
+        }))
+    }
+})
