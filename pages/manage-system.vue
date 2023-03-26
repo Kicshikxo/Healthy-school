9f36abb2f3707d9e0e22135a4046321317382d23
Kicshikxo <supermegavalera9000@gmail.com>
2023-03-26 21:15:07 +0300

    Add manage system pages

---
+++ pages/manage-system.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <p-tab-menu :model="items" />
+        <nuxt-layout :name="false">
+            <nuxt-page />
+        </nuxt-layout>
+    </div>
+</template>
+
+<script setup lang="ts">
+definePageMeta({
+    title: 'Управление системой'
+})
+
+const items = ref([
+    {
+        label: 'Пользователи',
+        icon: 'pi pi-fw pi-user',
+        to: '/manage-system/overview1'
+    }
+])
+</script>
---
+++ pages/manage-system/overview[id].vue
@@ -0,0 +1,8 @@
+<template>
+    <div>{{ $route.params.id }}</div>
+</template>
+<script setup lang="ts">
+definePageMeta({
+    title: 'Overview'
+})
+</script>
