5a6cf6fa760cd0f705ea9fad0ca8b8332e9d984e
Kicshikxo <supermegavalera9000@gmail.com>
2023-03-26 21:09:22 +0300

    Add check username endpoint

---
+++ server/api/users/check-username.get.ts
@@ -0,0 +1,26 @@
+import { PrismaClient, Role } from '@prisma/client'
+
+const prisma = new PrismaClient()
+
+export default defineEventHandler(async (event) => {
+    if (!checkRole(event, { roles: [Role.OPERATOR, Role.SCHOOL_OPERATOR] })) return
+
+    const { username } = getQuery(event) as { username: string }
+
+    if (!username)
+        return sendError(
+            event,
+            createError({
+                statusCode: 400,
+                statusMessage: 'username is not provided'
+            })
+        )
+
+    return {
+        available: !(await prisma.user.findUnique({
+            where: {
+                username: username
+            }
+        }))
+    }
+})
