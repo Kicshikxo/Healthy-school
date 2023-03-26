da548d088df242e0ebd538ac06849f70684221f1
Kicshikxo <supermegavalera9000@gmail.com>
2023-03-26 21:06:26 +0300

    Add organization info endpoint

---
+++ server/api/organizations/info.get.ts
@@ -0,0 +1,25 @@
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
+
+export default defineEventHandler(async (event) => {
+    const tokenData = readTokenData(event)
+    if (!tokenData) return
+
+    const query = getQuery(event) as { organizationId: string }
+
+    if (!query.organizationId)
+        return sendError(
+            event,
+            createError({
+                statusCode: 400,
+                statusMessage: 'organizationId is not provided'
+            })
+        )
+
+    return await prisma.educationalOrganization.findFirst({
+        where: {
+            id: query.organizationId
+        }
+    })
+})
