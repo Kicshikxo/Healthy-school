d8143a2a5fffcf7a45980dab947542a253335082
Kicshikxo <supermegavalera9000@gmail.com>
2023-03-26 21:02:45 +0300

    Add add student manage component

---
+++ components/manage/school/add/student.vue
@@ -0,0 +1,142 @@
+<template>
+    <manage-form
+        success-submit-summary="Успешное добавление"
+        error-submit-summary="Ошибка добавления"
+        submit-label="Добавить"
+        :on-submit="submit"
+        :on-cancel="resetForm"
+    >
+        <template #title> Добавление учащегося </template>
+        <template #form>
+            <manage-form-select-class
+                label="Класс"
+                placeholder="Выберите класс"
+                v-model="selectedClass"
+                :error="selectedClassError"
+                :organization-id="data?.organizationId"
+            />
+            <manage-form-input-mask
+                label="СНИЛС"
+                placeholder="Введите СНИЛС"
+                v-model="snils"
+                :error="snilsError"
+                mask="999-999-999 99"
+            />
+            <manage-form-input-text
+                label="Фамилия"
+                placeholder="Введите фамилию"
+                v-model="secondName"
+                :error="secondNameError"
+            />
+            <manage-form-input-text label="Имя" placeholder="Введите имя" v-model="firstName" :error="firstNameError" />
+            <manage-form-input-text
+                label="Отчество"
+                placeholder="Введите отчество"
+                v-model="middleName"
+                :error="middleNameError"
+            />
+            <div class="formgrid grid">
+                <manage-form-input-calendar
+                    label="Дата рождения"
+                    placeholder="Введите дату рождения"
+                    v-model="birthdate"
+                    :error="birthdateError"
+                    class="col"
+                />
+                <manage-form-input-dropdown
+                    label="Пол"
+                    placeholder="Выберите пол"
+                    v-model="gender"
+                    :error="genderError"
+                    :options="[
+                        { label: 'Мужской', value: 'MALE' },
+                        { label: 'Женский', value: 'FEMALE' }
+                    ]"
+                    optionLabel="label"
+                    optionValue="value"
+                    class="col"
+                />
+            </div>
+        </template>
+    </manage-form>
+</template>
+
+<script setup lang="ts">
+import { Class, Gender, Student } from '@prisma/client'
+import { useField, useForm } from 'vee-validate'
+
+const { resetForm, validate, setFieldError } = useForm()
+
+const { value: selectedClass, errorMessage: selectedClassError } = useField<Class>('class', (value: Class) => {
+    if (!value) return 'Выберите класс'
+    return true
+})
+const { value: snils, errorMessage: snilsError } = useField('snils', (value: string) => {
+    if (!validateSnils(value)) return 'Неверный формат снилса'
+    return true
+})
+const { value: secondName, errorMessage: secondNameError } = useField('secondName', (value?: string) => {
+    if (!value?.trim()) return 'Введите фамилию'
+    if (!/^[А-ЯЁ][а-яё]+$/.test(value)) return 'Неверный формат фамилии'
+    return true
+})
+const { value: firstName, errorMessage: firstNameError } = useField('firstName', (value?: string) => {
+    if (!value?.trim()) return 'Введите имя'
+    if (!/^[А-ЯЁ][а-яё]+$/.test(value)) return 'Неверный формат имени'
+    return true
+})
+const { value: middleName, errorMessage: middleNameError } = useField('middleName', (value?: string) => {
+    if (!value?.trim()) return 'Введите отчество'
+    if (!/^[А-ЯЁ][а-яё]+$/.test(value)) return 'Неверный формат отчества'
+    return true
+})
+const { value: gender, errorMessage: genderError } = useField('gender', (value: Gender) => {
+    if (!value) return 'Выберите пол'
+    if (![Gender.MALE, Gender.FEMALE].includes(value)) return 'Неверный пол'
+    return true
+})
+const { value: birthdate, errorMessage: birthdateError } = useField('birthdate', (value: Date) => {
+    if (!value) return 'Выберите дату рождения'
+    if (value > new Date()) return 'Дата рождения не может быть в будущем'
+    return true
+})
+
+const { data } = useAuthState()
+
+async function submit() {
+    const { valid } = await validate()
+    if (!valid) {
+        throw new Error('Форма имеет ошибки заполнения')
+    }
+
+    const { data: check } = await useFetch('/api/students/check-available', {
+        query: {
+            snils: snils.value
+        }
+    })
+    if (!check.value?.available) {
+        setFieldError('snils', 'Учащийся с таким СНИЛСом уже существует')
+        throw new Error('Учащийся с таким СНИЛСом уже существует')
+    }
+
+    const { error } = await useFetch('/api/students/add', {
+        method: 'POST',
+        body: {
+            studentData: {
+                snils: snils.value,
+                secondName: secondName.value,
+                firstName: firstName.value,
+                middleName: middleName.value,
+                gender: gender.value,
+                birthdate: birthdate.value,
+                classId: selectedClass.value.id
+            } as Student
+        }
+    })
+    if (error.value) {
+        throw new Error(error.value.message)
+    }
+
+    return 'Учащийся успешно добавлен'
+}
+</script>
