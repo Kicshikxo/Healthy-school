feeb8bffb8a0d959d4a017fc4719e321c76dc5ee
Kicshikxo <supermegavalera9000@gmail.com>
2023-03-29 12:30:11 +0300

    Return snils checksum check

--- utils/validations.ts
+++ utils/validations.ts
@@ -58,15 +58,15 @@ export const validateClassLiter = (liter?: string) => {
 export const validateSnils = (snils: string) => {
     if (!/^\d{3}-\d{3}-\d{3}\s\d{2}$/.test(snils)) return 'Неверный формат СНИЛСа'
 
-    // const [value, checksum] = snils.split(' ').map((part) => parseInt(part.replace(/\D/g, '')))
-    // const sum = `${value}`.split('').reduce((acc, value, index) => acc + parseInt(value) * (9 - index), 0)
+    const [value, checksum] = snils.split(' ').map((part) => parseInt(part.replace(/\D/g, '')))
+    const sum = `${value}`.split('').reduce((acc, value, index) => acc + parseInt(value) * (9 - index), 0)
 
-    // const valid =
-    //     (sum < 100 && sum === checksum) ||
-    //     ((sum === 100 || sum === 101) && checksum === 0) ||
-    //     (sum > 101 && (sum % 101 === checksum || (sum % 101 === 100 && checksum === 0)))
+    const valid =
+        (sum < 100 && sum === checksum) ||
+        ((sum === 100 || sum === 101) && checksum === 0) ||
+        (sum > 101 && (sum % 101 === checksum || (sum % 101 === 100 && checksum === 0)))
 
-    // if (!valid) return 'Недействительный СНИЛС'
+    if (!valid) return 'Недействительный СНИЛС'
     return true
 }
 
