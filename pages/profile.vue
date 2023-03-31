80bee32246c8d83330729110ba7e3a9c3b39631a
Kicshikxo <supermegavalera9000@gmail.com>
2023-03-31 22:20:57 +0300

    Add change password in profile

--- pages/profile.vue
+++ pages/profile.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <section class="p-5">
+        <section class="p-5 pb-0">
             <div class="flex justify-content-between gap-8">
                 <div class="flex flex-auto gap-5 h-6rem">
                     <div class="flex flex-auto flex-column justify-content-between">
@@ -11,7 +11,7 @@
                                 {{ data?.middleName }}
                             </div>
                         </div>
-                        <div class="flex flex-wrap gap-5">
+                        <div class="flex gap-5">
                             <div class="flex flex-column">
                                 <div class="text-500">Роль</div>
                                 <div class="flex align-items-end mt-2 text-700">
@@ -37,16 +37,89 @@
                 </div>
             </div>
         </section>
+        <manage-form
+            success-submit-summary="Успешное изменение"
+            error-submit-summary="Ошибка изменения"
+            submit-label="Изменить"
+            :on-submit="submit"
+            :on-cancel="resetForm"
+        >
+            <template #title> Изменение пароля </template>
+            <template #form>
+                <div class="formgrid grid">
+                    <form-input-password
+                        label="Текущий пароль"
+                        placeholder="Введите текущий пароль"
+                        v-model="currentPassword"
+                        :errorMessage="currentPasswordError"
+                        class="col"
+                    />
+                    <form-input-password
+                        label="Новый пароль"
+                        placeholder="Введите новый пароль"
+                        v-model="newPassword"
+                        :errorMessage="newPasswordError"
+                        class="col"
+                    />
+                </div>
+                <div class="formgrid grid">
+                    <form-input-password
+                        label="Повтор нового пароля"
+                        placeholder="Повторите новый пароль"
+                        v-model="repeatNewPassword"
+                        :errorMessage="repeatNewPasswordError"
+                        class="col col-offset-6"
+                    />
+                </div>
+            </template>
+        </manage-form>
     </div>
 </template>
 
 <script setup lang="ts">
+import { useField, useForm } from 'vee-validate'
+
 definePageMeta({
     title: 'Профиль'
 })
 
 const { data } = useAuthState()
 const { signOut } = useAuth()
+const { resetForm, validate, setFieldError } = useForm()
+
+const { value: currentPassword, errorMessage: currentPasswordError } = useField('currentPassword', validatePassword)
+const { value: newPassword, errorMessage: newPasswordError } = useField('newPassword', validatePassword)
+const { value: repeatNewPassword, errorMessage: repeatNewPasswordError } = useField('repeatPassword', (value?: string) => {
+    if (!value?.trim()) return 'Повторите пароль'
+    if (value !== newPassword.value) return 'Пароли не совпадают'
+    if (value.length > 50) return 'Слишком длинный пароль'
+    return true
+})
+
+async function submit() {
+    const { valid } = await validate()
+    if (!valid) {
+        throw new Error('Форма имеет ошибки заполнения')
+    }
+
+    const { error } = await useFetch('/api/users/change-password', {
+        method: 'POST',
+        body: {
+            currentPassword: currentPassword.value,
+            newPassword: newPassword.value
+        }
+    })
+    if (error.value?.statusCode === 406) {
+        setFieldError('currentPassword', 'Неверный текущий пароль')
+        throw new Error('Неверный текущий пароль')
+    }
+    if (error.value) {
+        throw new Error(error.value.message)
+    }
+
+    resetForm()
+    return 'Пароль успешно изменён'
+}
 
 const { data: organization, error: errorOrganization } = useFetch('/api/organizations/info', {
     query: { organizationId: computed(() => data.value?.organizationId) },
---
+++ server/api/users/change-password.post.ts
@@ -0,0 +1,48 @@
+import { PrismaClient, Role } from '@prisma/client'
+import { compareSync, hashSync } from 'bcrypt'
+
+const prisma = new PrismaClient()
+
+export default defineEventHandler(async (event) => {
+    const tokenData = readTokenData(event)
+    if (!tokenData) return
+
+    const body: { userId: string; currentPassword: string; newPassword: string } = await readBody(event)
+
+    if (!body.newPassword)
+        return sendError(
+            event,
+            createError({
+                statusCode: 400,
+                statusMessage: 'newPassword is not provided'
+            })
+        )
+
+    let userId: string
+    if (body.userId && checkRole(event, { roles: [Role.OPERATOR, Role.SCHOOL_OPERATOR] })) {
+        userId = body.userId
+    } else {
+        userId = tokenData.id
+    }
+
+    const user = await prisma.user.findUnique({ where: { id: userId } })
+
+    if (
+        (body.userId && checkRole(event, { roles: [Role.OPERATOR, Role.SCHOOL_OPERATOR] })) ||
+        !compareSync(body.currentPassword, user?.password ?? '')
+    )
+        return sendError(
+            event,
+            createError({
+                statusCode: 406,
+                statusMessage: 'currentPassword does not match'
+            })
+        )
+
+    return await prisma.user.update({
+        where: { id: userId },
+        data: {
+            password: hashSync(body.newPassword, 8)
+        }
+    })
+})
