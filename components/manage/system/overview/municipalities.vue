5a5ff54da2369b09771d0310a5f42fe35cba1d46
Kicshikxo <supermegavalera9000@gmail.com>
2023-04-07 17:06:28 +0300

    Create overview municipalities component

---
+++ components/manage/system/overview/municipalities.vue
@@ -0,0 +1,30 @@
+<template>
+    <p-data-table
+        :value="municipalities"
+        :rowHover="!!municipalities?.length"
+        :scrollable="true"
+        scrollHeight="flex"
+        dataKey="id"
+        responsiveLayout="scroll"
+        class="p-datatable-lg"
+    >
+        <template #empty>
+            <div class="flex justify-content-center w-full h-1rem">
+                <p-progress-bar v-if="loadingMunicipalities" mode="indeterminate" class="w-full h-1rem" />
+                <div v-else>Муниципалитетов нет...</div>
+            </div>
+        </template>
+        <p-column field="name" header="Название" />
+        <p-column field="_count.organizations" header="Количество школ" />
+    </p-data-table>
+</template>
+
+<script setup lang="ts">
+const {
+    data: municipalities,
+    pending: loadingMunicipalities,
+    refresh: refreshMunicipalities
+} = useFetch('/api/municipalities/list', {
+    headers: useRequestHeaders() as HeadersInit
+})
+</script>
