f6b2d4f35d0cc11f13bea5c82e180c35dc0a4a19
Kicshikxo <supermegavalera9000@gmail.com>
2023-04-07 17:06:35 +0300

    Create overview organizations component

---
+++ components/manage/system/overview/organizations.vue
@@ -0,0 +1,46 @@
+<template>
+    <p-data-table
+        :value="organizations"
+        :rowHover="!!organizations?.length"
+        :scrollable="true"
+        scrollHeight="flex"
+        dataKey="id"
+        responsiveLayout="scroll"
+        class="p-datatable-lg"
+    >
+        <template #header>
+            <div class="flex">
+                <form-select-municipality
+                    placeholder="Выберите муниципалитет"
+                    v-model="selectedMunicipality"
+                    hideErrorMessage
+                    class="mb-0 max-w-20rem white-space-nowrap"
+                />
+            </div>
+        </template>
+        <template #empty>
+            <div class="flex justify-content-center w-full h-1rem">
+                <p-progress-bar v-if="loadingOrganizations" mode="indeterminate" class="w-full h-1rem" />
+                <div v-else>Школ нет...</div>
+            </div>
+        </template>
+        <p-column field="name" header="Название" />
+        <p-column field="_count.classes" header="Количество классов" />
+    </p-data-table>
+</template>
+
+<script setup lang="ts">
+import { Municipality } from '@prisma/client'
+
+const selectedMunicipality = ref<Municipality>()
+
+const {
+    data: organizations,
+    pending: loadingOrganizations,
+    refresh: refreshOrganizations
+} = useFetch('/api/organizations/list', {
+    query: { municipalityId: computed(() => selectedMunicipality.value?.id) },
+    headers: useRequestHeaders() as HeadersInit,
+    immediate: !!selectedMunicipality.value
+})
+</script>
