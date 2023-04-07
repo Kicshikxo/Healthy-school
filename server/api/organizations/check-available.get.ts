1e82cb5e08861061e8bc0e12fcef883d9eba0742
Kicshikxo <supermegavalera9000@gmail.com>
2023-04-07 17:07:36 +0300

    Create check-available organization endpoint

---
+++ server/api/organizations/check-available.get.ts
@@ -0,0 +1,33 @@
+import { PrismaClient, Role } from '@prisma/client'
+
+const prisma = new PrismaClient()
+
+export default defineEventHandler(async (event) => {
+    if (!checkRole(event, { roles: [Role.OPERATOR] })) return
+
+    const query = getQuery(event) as {
+        name: string
+        skipId?: string
+    }
+
+    if (!query.name)
+        return sendError(
+            event,
+            createError({
+                statusCode: 400,
+                statusMessage: 'name is not provided'
+            })
+        )
+
+    return {
+        available: !(await prisma.educationalOrganization.findFirst({
+            where: {
+                id: { not: query.skipId },
+                name: {
+                    equals: query.name,
+                    mode: 'insensitive'
+                }
+            }
+        }))
+    }
+})
