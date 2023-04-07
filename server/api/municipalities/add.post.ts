73cb0dfc15747b753f98893154b93bfbd0b54ba4
Kicshikxo <supermegavalera9000@gmail.com>
2023-04-07 17:06:49 +0300

    Create add municipality endpoint

---
+++ server/api/municipalities/add.post.ts
@@ -0,0 +1,39 @@
+import { ActionType, PrismaClient, Role } from '@prisma/client'
+
+const prisma = new PrismaClient()
+
+export default defineEventHandler(async (event) => {
+    if (!checkRole(event, { roles: [Role.OPERATOR] })) return
+
+    const body: { name: string } = await readBody(event)
+
+    if (!body.name)
+        return sendError(
+            event,
+            createError({
+                statusCode: 400,
+                statusMessage: 'name is not provided'
+            })
+        )
+
+    return await prisma.municipality
+        .create({
+            data: {
+                name: body.name
+            }
+        })
+        .then((data) =>
+            prisma.actionLog.create({
+                data: {
+                    createdById: readTokenData(event)!.id,
+                    actionType: ActionType.CREATE,
+                    details: JSON.parse(
+                        JSON.stringify({
+                            action: 'createMunicipality',
+                            data
+                        })
+                    )
+                }
+            })
+        )
+})
