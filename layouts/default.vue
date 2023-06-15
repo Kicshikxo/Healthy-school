1337a6b98dc091fad0833a25abf12e19f7e0504d
Kicshikxo <supermegavalera9000@gmail.com>
2023-06-15 06:18:59 +0300

    Fix loading indicator

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,7 +2,7 @@
     <div class="flex flex-column min-h-screen surface-ground">
         <div class="h-16rem bg-primary-500">
             <app-header />
-            <nuxt-loading-indicator color="var(--primary-600)" class="mt-7 shadow-2" :height="2" />
+            <nuxt-loading-indicator color="var(--primary-600)" class="mt-7 shadow-2" :height="2" :throttle="0" />
             <app-nav />
         </div>
 
