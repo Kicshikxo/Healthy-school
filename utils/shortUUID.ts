6906409d999c55751efab0dd1d008cc0c8e98318
Kicshikxo <41877938+Kicshikxo@users.noreply.github.com>
2023-06-25 04:46:41 +0300

    Fix UUID translation with incorrect input data

--- utils/shortUUID.ts
+++ utils/shortUUID.ts
@@ -2,5 +2,17 @@ import short from 'short-uuid'
 
 const translator = short()
 
-export const shortUUID = (uuid: string) => translator.fromUUID(uuid)
-export const parseUUID = (shortUUID: string) => translator.toUUID(shortUUID)
+export const shortUUID = (uuid: string) => {
+    try {
+        return translator.fromUUID(uuid)
+    } catch(e) {
+        return translator.fromUUID('')
+    }
+}
+export const parseUUID = (shortUUID: string) => {
+    try {
+        return translator.toUUID(shortUUID)
+    } catch(e) {
+        return translator.toUUID('')
+    }
+}
